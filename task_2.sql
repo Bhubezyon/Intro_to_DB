08064763f7ef74b0790538f9b3d7788b3e3eec35
bhubezyon <bhubezyon@gmail.com>
2025-08-17 10:26:19 -0700

    Add task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -5,32 +5,32 @@ CREATE TABLE Authors (author_id INT AUTO_INCREMENT PRIMARY KEY,
                       author_name VARCHAR(200) NOT NULL,
                       BIO TEXT);
 
-CREATE TABLE Books (BOOK_ID INT AUTO_INCREMENT PRIMARY KEY,
-                    TITLE VARCHAR(255) NOT NULL,
-                    AUTHOR_ID INT NOT NULL,
-                    GENRE VARCHAR(100),
-                    PRICE DECIMAL(10, 2),
-                    STOCK INT DEFAULT 0,
+CREATE TABLE Books (book_id INT AUTO_INCREMENT PRIMARY KEY,
+                    title VARCHAR(255) NOT NULL,
+                    author INT NOT NULL,
+                    genre VARCHAR(100),
+                    price DECIMAL(10, 2),
+                    stock INT DEFAULT 0,
                     FOREIGN KEY (AUTHOR_ID) REFERENCES AUTHORS(AUTHOR_ID));
 
-CREATE TABLE Customers (CUSTOMER_ID INT AUTO_INCREMENT PRIMARY KEY,
-                        FIRST_NAME VARCHAR(100) NOT NULL,
-                        LAST_NAME VARCHAR(100) NOT NULL,
-                        EMAIL VARCHAR(255) UNIQUE NOT NULL,
-                        PHONE VARCHAR(20),
+CREATE TABLE Customers (customer_id INT AUTO_INCREMENT PRIMARY KEY,
+                        first_time VARCHAR(100) NOT NULL,
+                        last_name VARCHAR(100) NOT NULL,
+                        email VARCHAR(255) UNIQUE NOT NULL,
+                        phone VARCHAR(20),
                         ADDRESS TEXT);
 
-CREATE TABLE Orders (ORDER_ID INT AUTO_INCREMENT PRIMARY KEY,
-                     CUSTOMER_ID INT NOT NULL,
-                     ORDER_DATE DATETIME DEFAULT CURRENT_TIME STAMP,
+CREATE TABLE Orders (order_id INT AUTO_INCREMENT PRIMARY KEY,
+                     customer_id INT NOT NULL,
+                     order_date DATETIME DEFAULT CURRENT_TIME STAMP,
                      STATUS VARCHAR(50) DEFAULT 'PENDING',
                      FOREIGN KEY (CUSTOMER_ID) REFERENCES CUSTOMER(CUSTOMER_ID)
                      );
 
-CREATE TABLE Order_details (ORDER_DETAIL_ID INT AUTO PRIMARY KEY,
-                            ORDER_ID INT NOT NULL,
-                            BOOK_ID INT NOT NULL,
-                            QUANTITY INT NOT NULL,
-                            UNIT_PRICE DECIMAL(10, 2) NOT NULL,
+CREATE TABLE Order_details (order_detail_id INT AUTO PRIMARY KEY,
+                            order_id INT NOT NULL,
+                            book_id INT NOT NULL,
+                            quantity INT NOT NULL,
+                            unit_price DECIMAL(10, 2) NOT NULL,
                             FOREIGN KEY (ORDER_ID) REFERENCES ORDERS(ORDER_ID),
                             FOREIGN KEY (BOOK_ID) REFERNCES BOOKS(BOOK_ID));
