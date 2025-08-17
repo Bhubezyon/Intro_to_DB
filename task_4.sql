4d1eeab099eb1276afa2c8dc6306016538b3e33c
bhubezyon <bhubezyon@gmail.com>
2025-08-17 12:10:24 -0700

    Add task_4

---
+++ task_4.sql
@@ -0,0 +1,6 @@
+-- task_4.sql
+--Prints full column details of the 'books' table from 'alx_book_store'
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books'
